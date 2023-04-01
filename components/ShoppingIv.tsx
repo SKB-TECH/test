f1cc9862f4668cfa63ddd981258919420584f283
Shako Kinyamba <shakokinyamba201@gmail.com>
2023-04-01 23:09:49 +0200

    Refact: components

--- components/ShoppingIv.tsx
+++ components/ShoppingIv.tsx
@@ -1,31 +1,34 @@
 import React from 'react'
 import Card_shopping from './s_component/Card_shopping'
 import shoppingDataII from '@/public/data/dataShoppingII'
+import shoppingDataV from '../public/data/dataShoppingIv';
 
 export default function ShoppingIv() {
   return (
     <section className='flex flex-col bg-d_jaune min-h-fit min-w-full'>
     <div className='flex flex-col lg:justify-around items-center lg:flex-row h-fit min-w-full mt-5 border-b-2 border-gray-400'>
-        <span className='text-footer lg:text-lg  text-2xl font-beatrice mb-2'>Premium quality cocoa</span>
-        <span className='text-footer lg:text-lg  text-2xl font-beatrice mb-2'>natural ingredients</span>
-        <span className='text-footer lg:text-lg  text-2xl font-beatrice'>Focus-enhancing treat</span>
-        <span className='text-footer lg:text-lg  text-2xl font-beatrice mb-2'>Premium quality cocoa</span>
-        <span className='text-footer lg:text-lg  text-2xl font-beatrice mb-2'>natural ingredients</span>
-        <span className='text-footer lg:text-lg  text-2xl font-beatrice mb-2'>Focus-enhancing treat</span>
-    
+        <span className='text-footer lg:text-lg  text-2xl font-beatrice mb-2'>Convenient</span>
+        <span className='text-footer lg:text-lg  text-2xl font-beatrice mb-2'>enhances focus</span>
+        <span className='text-footer lg:text-lg  text-2xl font-beatrice'>creativity</span>
+        <span className='text-footer lg:text-lg  text-2xl font-beatrice mb-2'>Convenient</span>
+        <span className='text-footer lg:text-lg  text-2xl font-beatrice mb-2'>enhances focus</span>
+        <span className='text-footer lg:text-lg  text-2xl font-beatrice mb-2'>creativity</span>
+        <span className='text-footer lg:text-lg  text-2xl font-beatrice'>Convenient</span>
+        <span className='text-footer lg:text-lg  text-2xl font-beatrice mb-2'>enhances focus</span>
+        
     </div>
     {/* <hr  className='h-1 text-gray-400'></hr> */}
     <div className='flex flex-col justify-around lg:flex-row min-w-full h-42 border-b-2 border-gray-400 p-5'>
         <h1 className='font-sunwish text-2xl lg:text-3xl lg:text-left text-center text-footer'>
-        Magic Amanita <br></br> Mushroom Gummy
+        Purple Shroom Cone
         </h1>
         <p className="text-footer text-1xl lg:text-2xl font-beatrice font-normal">
-        Elevate your well-being with our psilocybin-free, <br></br> vegan, and gluten-free mushroom <br></br>gummies is perfect for busy lifestyles
+        Simply light up, take a few puffs,<br></br> and let the stress melt away
         </p>
     </div>
     <div className='flex flex-col lg:flex-row justify-between min-h-full min-w-full lg:border-b-2 border-gray-400 p-5 lg:p-0'>
         {
-          shoppingDataII.map((items,index)=>(
+          shoppingDataV.map((items,index)=>(
             <Card_shopping  title={items.title} image={items.picture} key={index}/>
           ))
         }
---
+++ public/data/dataShoppingIv.ts
@@ -0,0 +1,22 @@
+import logo from '../logo2.svg'
+
+
+const shoppingDataV=[
+    {
+        picture: logo,
+        title: "PURPLE SHROOM CONE MEADOWS RED",
+        
+    },
+    {
+        picture: logo,
+        title: "PURPLE SHROOM CONE BUZZ BERRY",
+        
+    },
+    {
+        picture:logo,
+        title: "PURPLE SHROOM CONE WATERMELON MADNESS",
+      
+    }
+]
+
+export default shoppingDataV;
